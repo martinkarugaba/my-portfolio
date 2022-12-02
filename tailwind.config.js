5eea3ad882cda02ed71620fb4f77165c2e89e5ec
Martin Karugaba <martinkarugaba21@gmail.com>
2022-12-02 14:26:04 +0300

    add header styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,24 @@ module.exports = {
       colors: {
         'light-blue': '#6070FF',
         'dark-blue': '#172B4D',
+        'light-teal': '#344563',
+        h2: '#7F8CFF',
       },
     },
-    fontFamily:{
-      poppins:['Poppins','sans-serif']
-    }
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+    },
+    backgroundImage: {
+      'hero-shapes': "url('/src/img/header-shapes-mobile.png')",
+    },
+    fontSize: {
+      40: '40px',
+    },
+    lineHeight: {
+      11: '50px',
+      12: '60px',
+      10:'10px'
+    },
   },
   plugins: [],
 };
