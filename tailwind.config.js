f07703529655c15e54820b8f5f6c07943da349a8
Martin Karugaba <martinkarugaba21@gmail.com>
2022-12-02 12:31:23 +0300

    add custom font poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,12 @@ module.exports = {
     extend: {
       colors: {
         'light-blue': '#6070FF',
+        'dark-blue': '#172B4D',
       },
     },
+    fontFamily:{
+      poppins:['Poppins','sans-serif']
+    }
   },
   plugins: [],
 };
