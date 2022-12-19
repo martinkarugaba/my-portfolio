371b73ea006e34d0f411158509b997ac7a6a4692
Martin Karugaba <martinkarugaba21@gmail.com>
2022-12-19 21:17:05 +0300

    make mobile-nav disappear when nav-link is clicked

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const bars = document.querySelector('.bars');
 const mobile_menu = document.querySelector('.mobile-menu');
 const close_btn = document.querySelector('.close-btn');
-
+const nav_links = document.querySelectorAll('.nav-link');
 
 bars.addEventListener('click', () => {
-  mobile_menu.classList.add('open')
-})
+  mobile_menu.classList.add('open');
+});
 
 close_btn.addEventListener('click', () => {
   mobile_menu.classList.remove('open');
 });
 
+nav_links.forEach((nav_link) => {
+  mobile_menu.addEventListener('click', () => {
+    mobile_menu.classList.remove('open');
+  });
+});
