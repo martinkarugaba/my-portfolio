ba3565aabc5790162f2b718b830f1bf60e806da2
mohi <mohisadeghi302@gmail.com>
2022-12-20 13:56:13 +0330

    Add projects samples

--- projects.js
+++ projects.js
@@ -4,7 +4,7 @@ const projectsList = [
   {
     name: 'Tonic',
     subtitle: ['canopy', 'back end dev', '2015'],
-    'featured-image': 'img/desktop/project-1.jpg',
+    featuredImage: 'img/desktop/project-1.jpg',
     description:
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     button: 'see prooject',
@@ -13,7 +13,7 @@ const projectsList = [
   {
     name: 'Multi-post stories',
     subtitle: ['canopy', 'back end dev', '2015'],
-    'featured-image': 'img/desktop/project-2.jpg',
+    featuredImage: 'img/desktop/project-2.jpg',
     description:
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     button: 'see prooject',
@@ -22,7 +22,7 @@ const projectsList = [
   {
     name: 'Tonic',
     subtitle: ['canopy', 'back end dev', '2015'],
-    'featured-image': 'img/desktop/project-3.jpg',
+    featuredImage: 'img/desktop/project-3.jpg',
     description:
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     button: 'see prooject',
@@ -31,7 +31,7 @@ const projectsList = [
   {
     name: 'Tonic',
     subtitle: ['canopy', 'back end dev', '2015'],
-    'featured-image': 'img/desktop/project-4.jpg',
+    featuredImage: 'img/desktop/project-4.jpg',
     description:
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     button: 'see prooject',
@@ -40,9 +40,80 @@ const projectsList = [
 ];
 
 projectsList.forEach((project) => {
-  //const { name, subtitle, img, description, button, badges } =
-  //  project;
-  projectsSection.innerHTML += `<h3>${project.name}</h3>`;
+  const { name, subtitle, featuredImage, description, button, technologies} = project;
+  projectsSection.innerHTML += ` <div
+  class="w-[100%] p-4 md:p-[24px] border md:order-1 md:h-[496px] md:gap-x-[2.9rem] border-light-grey-2 bg-white rounded-2xl flex flex-col md:flex-row justify-center items-center"
+>
+  <!--* project screen shot -->
+  <div class="w-full md:w-[50%] h-[50%] md:h-full bg-white">
+    <img
+      class="w-full h-full object-cover md:hover:scale-110 transition duration-700 ease-in-out"
+      src=${featuredImage}
+      alt="project-1"
+    />
+  </div>
+  <!--* project info -->
+
+  <div
+    class="flex md:w-[50%] md:h-full flex-col justify-around md:justify-start items-start gap-y-4"
+  >
+    <h2
+      class="text-[32px] md:text-[40px] text-dark-blue font-bold mt-3 -mb-2"
+    >
+      ${name}
+    </h2>
+    <ul
+      class="list-none flex justify-between md:text-lg items-center w-[240px]"
+    >
+      <li
+        class="text-light-dark-blue md:text-lg text-small font-semibold"
+      >
+      ${subtitle[0]}
+      </li>
+      <li class="h-2 w-2 bg-light-grey rounded-full"></li>
+      <li class="text-light-grey-4 font-semibold text-small">
+      ${subtitle[1]}
+      </li>
+      <li class="h-2 w-2 bg-light-grey rounded-full"></li>
+      <li class="text-light-grey-4 font-semibold text-small">
+      ${subtitle[2]}
+      </li>
+    </ul>
+    <p class="text-light-teal md:text-base text-[15px]">
+      ${description}
+    </p>
+
+    <ul class="w-[230px] flex justify-between items-center">
+      <li>
+        <a
+          href="#"
+          class="text-light-blue text-xs px-3 py-1 bg-faded-blue rounded-lg"
+          >${technologies[0]}</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="text-light-blue text-xs px-3 py-1 bg-faded-blue rounded-lg"
+          >${technologies[1]}</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="text-light-blue text-xs px-3 py-1 bg-faded-blue rounded-lg"
+          >${technologies[2]}</a
+        >
+      </li>
+    </ul>
+    <a
+      href="#"
+      class="inline-block transition duration-500 ease-in-out text-primary-blue p-4 text-[17px] border border-light-blue rounded-lg mb-[14px] md:enabled:text-light-blue md:enabled:border-[#7F8CFF] md:enabled:bg-white md:hover:bg-light-blue md:hover:text-white md:active:bg-[#2230D2] md:active:text-white md:disabled:bg-[#e5e5e5] md:disabled:text-[#5E6C84] md:disabled:border-[#C1C7D0]"
+      >${button}</a
+    >
+  </div>
+</div>`;
+
 });
 
 console.log('projects section');
