512d4327bae2e23d800e92f8944c94d661347b70
Martin Karugaba <martinkarugaba21@gmail.com>
2022-12-22 09:39:08 +0300

    fix linter errors

--- projects.js
+++ projects.js
@@ -11,8 +11,6 @@ const projectsList = [
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     button: 'see project',
     technologies: ['html', 'css', 'javascript'],
-    description:
-      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     modalDescription:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     modalButtons: ['see live', 'see source'],
@@ -26,8 +24,6 @@ const projectsList = [
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     button: 'see project',
     technologies: ['html', 'css', 'javascript'],
-    description:
-      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     modalButtons: ['see live', 'see source'],
     modalDescription:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
@@ -41,8 +37,6 @@ const projectsList = [
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     button: 'see project',
     technologies: ['html', 'css', 'javascript'],
-    description:
-      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     modalButtons: ['see live', 'see source'],
     modalDescription:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
