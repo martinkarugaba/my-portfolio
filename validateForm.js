c6f4ddfd2d752f51c95e10d234ec9004654cb576
Martin Karugaba <martinkarugaba21@gmail.com>
2022-12-22 13:50:52 +0300

    fix linter errors

--- validateForm.js
+++ validateForm.js
@@ -2,14 +2,12 @@ const contactForm = document.querySelector('.contact-form');
 const email = document.querySelector('#email');
 const errorContainer = document.querySelector('small');
 
-
 contactForm.addEventListener('submit', (e) => {
   e.preventDefault();
   if (email.value !== email.value.toLowerCase()) {
-    email.classList.add("error");
-    errorContainer.classList.remove("hidden");
-  }
-  else {
+    email.classList.add('error');
+    errorContainer.classList.remove('hidden');
+  } else {
     contactForm.submit();
   }
 });
