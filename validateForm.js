74176752f7cbebe2ac46f864c4159acca4c11ad5
mohi <mohisadeghi302@gmail.com>
2022-12-22 13:19:50 +0330

    Add email lowercase validation

--- validateForm.js
+++ validateForm.js
@@ -1,5 +1,15 @@
 const contactForm = document.querySelector('.contact-form');
+const email = document.querySelector('#email');
+const errorContainer = document.querySelector('small');
+
 
 contactForm.addEventListener('submit', (e) => {
   e.preventDefault();
+  if (email.value !== email.value.toLowerCase()) {
+    email.classList.add("error");
+    errorContainer.classList.remove("hidden");
+  }
+  else {
+    contactForm.submit();
+  }
 });
